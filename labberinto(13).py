c4e66d42878727a7e88fe021953fd1a595765a90
javiipzo <javipozo999@gmail.com>
2022-12-16 19:48:40 +0100

    bj

--- labberinto(13).py
+++ labberinto(13).py
@@ -2,7 +2,6 @@ labr=[" "," ","X","X","X","X","X","X","X","X","X"],["X"," ","X","X","X","X"," ",
 for i in range (len(labr)):
     
     print(labr[i])
-    print(len(labr))
 
 def resolverlab(lab):
     resolver=[]
@@ -11,35 +10,45 @@ def resolverlab(lab):
     resolver.append((0,0))
     resuelto= False
     last_mov=""
+    yamovido=False
     while not resuelto:
         if posx==9 and posy==10:
             resuelto=True
             break
+        yamovido=False
 
-        if lab[posx][posy+1]==' ' and last_mov!="Izquierda":
-            print('Moviendo a la derecha')
-            posy+=1
-            last_mov="Derecha"
-            resolver.append((posx,posy))
-            print(posx,posy)
-        elif lab[posx+1][posy]==' ' and last_mov!="Arriba":
-            print('Moviendo hacia abajo')
-            posx+=1
-            last_mov="Abajo"
-            resolver.append((posx,posy))
-            print(posx,posy)
-        elif lab[posx][posy-1]==' ' and last_mov!="Derecha":
-            print('Moviendo hacia la izquierda')
-            posy-=1
-            last_mov="Izquierda"
-            resolver.append((posx,posy))
-            print(posx,posy)
-        elif lab[posx-1][posy]==' ' and last_mov!="Abajo":
-            print('Moviendo hacia arriba')
-            posx-=1
-            last_mov="Arriba"
-            resolver.append((posx,posy))
-            print(posx,posy)
+        if posy+1<len(lab[0]) and not yamovido:
+            if lab[posx][posy+1]==' ' and last_mov!="Izquierda":
+                print('Moviendo a la derecha')
+                posy+=1
+                last_mov="Derecha"
+                resolver.append((posx,posy))
+                print(posx,posy)
+                yamovido=True
+        if posx+1<len(lab) and not yamovido:
+            if lab[posx+1][posy]==' ' and last_mov!="Arriba":
+                print('Moviendo hacia abajo')
+                posx+=1
+                last_mov="Abajo"
+                resolver.append((posx,posy))
+                print(posx,posy)
+                yamovido=True
+        if posy-1>=0 and not yamovido:
+            if lab[posx][posy-1]==' ' and last_mov!="Derecha":
+                print('Moviendo hacia la izquierda')
+                posy-=1
+                last_mov="Izquierda"
+                resolver.append((posx,posy))
+                print(posx,posy)
+                yamovido=True
+        if posx-1>=0 and not yamovido:
+            if lab[posx-1][posy]==' ' and last_mov!="Abajo":
+                print('Moviendo hacia arriba')
+                posx-=1
+                last_mov="Arriba"
+                resolver.append((posx,posy))
+                print(posx,posy)
+                yamovido=True
     return resolver
     
 
--- prueba.py
+++ prueba.py
@@ -1,2 +1,6 @@
-for i in range(-2,3,3):
-    print(i)
+lista=[1,4,6,3,6,2]
+print(lista)
+lista[1]=98
+print(lista)
+lista[6]=789
+print(lista)
